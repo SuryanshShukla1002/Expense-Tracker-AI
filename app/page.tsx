7b9e317ac6c9f84165a59a62bd8c431e09f9bf22
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-10-09 02:40:51 +0530

    Render guest or home view based on authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
-export default function page() {
-  return <div className="text-red-400">Home Page</div>;
-} 
+import Guest from "@/components/Guest";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function page() {
+  const user = await currentUser();
+  if (!user) {
+    return <Guest />;
+  }
+  return (
+    <div className="text-red-800">
+      <h1>Home Page</h1>
+    </div>
+  );
+}
